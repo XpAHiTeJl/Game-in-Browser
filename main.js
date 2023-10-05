78c73b309e5dee9f7b467a31acd5607839bb2a40
XpAHiTeJl <robengud2@gmail.com>
2023-10-06 01:55:56 +0300

    login

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 import "./style.scss";
+import { App as Application } from "./App/App";
+import { Button, Input } from "./component/index";
+import { append, prepend } from "./utils/append";
+import { Component } from "./core/component";
+import { render } from "./core/render";
+
+const app = document.querySelector("body");
+
+const buttonsAutoriz = [
+  {
+    textContent: "Login",
+  },
+  {
+    textContent: "Register",
+  },
+].map((btn) => {
+  return new Button({
+    tagName: "button",
+    className: "btn",
+    textContent: btn.textContent,
+  });
+});
+
+const App = new Application({
+  tagName: "div",
+  className: "app",
+  children: [...buttonsAutoriz],
+}).toHTML();
+
+prepend(app, App);
